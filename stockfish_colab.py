17b1a8d55bc390e2820491080ea828de5753c1dd
eclipse <eclipses.na.terra@gmail.com>
2026-08-17 03:06:14 -0300

    Update stockfish_colab.py

--- stockfish_colab.py
+++ stockfish_colab.py
@@ -2,25 +2,15 @@ import match_runner
 
 # ----------------------
 START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-MAX_MOVES = 200
-RESIGN_CENTIPAWNS = 200
-PGN_NAME = "match_2008.pgn"
 
-WHITE_ENGINE = "/content/stockfish/stockfish-ubuntu-x86-64-avx2"
-WHITE_TIME = 1.0
-WHITE_DEPTH = 18
-WHITE_HASH = 248
+WHITE_ENGINE = "/content/stockfish/stockfish-linux-x86-64-universal"
+WHITE_DEPTHS = [(200, 18), (70, 20), (0, 18)]
+WHITE_HASH = 64
 WHITE_THREADS = 1
 
-BLACK_ENGINE = "/content/stockfish/stockfish-ubuntu-x86-64-avx2"
-BLACK_TIME = 1.0
-BLACK_DEPTH = 18
-BLACK_HASH = 248
+BLACK_ENGINE = "/content/stockfish/stockfish-linux-x86-64-universal"
+BLACK_DEPTHS = [(200, 18), (70, 20), (0, 18)]
+BLACK_HASH = 64
 BLACK_THREADS = 1
 # ----------------------
-
-match_runner.run_match(
-    START_FEN, MAX_MOVES, RESIGN_CENTIPAWNS, PGN_NAME,
-    WHITE_ENGINE, WHITE_TIME, WHITE_DEPTH, WHITE_HASH, WHITE_THREADS,
-    BLACK_ENGINE, BLACK_TIME, BLACK_DEPTH, BLACK_HASH, BLACK_THREADS
-)
+match_runner.run_match(START_FEN, WHITE_ENGINE, WHITE_HASH, WHITE_THREADS, WHITE_DEPTHS, BLACK_ENGINE, BLACK_HASH, BLACK_THREADS, BLACK_DEPTHS)
