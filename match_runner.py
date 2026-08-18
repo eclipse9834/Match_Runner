3637898082fcf668db710ad37b3b00c202808467
eclipse <eclipses.na.terra@gmail.com>
2026-08-18 01:15:56 -0300

    Update match_runner.py

--- match_runner.py
+++ match_runner.py
@@ -7,9 +7,9 @@ def run_match(f,o,we,wh,wt,wd,be,bh,bt,bd,pn):
     with e.SimpleEngine.popen_uci(we) as W, e.SimpleEngine.popen_uci(be) as B:
         W.configure({"Hash":wh,"Threads":wt}); B.configure({"Hash":bh,"Threads":bt})
         E,T,v = (B,W), (Bd,Wd), 0
-        while not b.is_game_over() and (res:=E[b.turn].analyse(b,e.Limit(depth=next((x for k,x in T[b.turn] if abs(v)>=k),10)))).get("pv"):
+        while not b.is_game_over() and (res:=E[b.turn].analyse(b,e.Limit(depth=(d:=next((x for k,x in T[b.turn] if abs(v)>=k),10))))).get("pv"):
             v,m = res["score"].white().score(mate_score=1e5) or 0, res["pv"][0]
+            print(f"{b.san(m)} | D:{d} | {v:+}")
             n = n.add_variation(m); b.push(m)
-        wn, bn = W.id.get("name", "White"), B.id.get("name", "Black")
-        g.headers.update({"Event":"Match","White":wn,"Black":bn,"Result":b.result(),"FEN":f,"SetUp":"1"})
+        g.headers.update({"Event":"Match","White":W.id.get("name","W"),"Black":B.id.get("name","B"),"Result":b.result(),"FEN":f,"SetUp":"1"})
         open(pn,"w").write(str(g))
