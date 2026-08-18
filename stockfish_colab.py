92a5261e2b55cd5f4034b2e5ebd7d7585c41344a
eclipse <eclipses.na.terra@gmail.com>
2026-08-17 21:36:56 -0300

    Update stockfish_colab.py

--- stockfish_colab.py
+++ stockfish_colab.py
@@ -2,6 +2,8 @@ import match_runner
 
 # ----------------------
 START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+OPENING_MOVES = ""
+PGN_NAME = "partida.pgn"
 
 WHITE_ENGINE = "/content/stockfish/stockfish-ubuntu-x86-64-avx2"
 WHITE_DEPTHS = [(200, 18), (70, 20), (0, 18)]
@@ -13,4 +15,4 @@ BLACK_DEPTHS = [(200, 18), (70, 20), (0, 18)]
 BLACK_HASH = 64
 BLACK_THREADS = 1
 # ----------------------
-match_runner.run_match(START_FEN, WHITE_ENGINE, WHITE_HASH, WHITE_THREADS, WHITE_DEPTHS, BLACK_ENGINE, BLACK_HASH, BLACK_THREADS, BLACK_DEPTHS)
+match_runner.run_match(START_FEN, OPENING_MOVES, PGN_NAME, WHITE_ENGINE, WHITE_HASH, WHITE_THREADS, WHITE_DEPTHS, BLACK_ENGINE, BLACK_HASH, BLACK_THREADS, BLACK_DEPTHS)
