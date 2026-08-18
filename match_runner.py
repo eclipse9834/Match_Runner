80fea2e2309da3ad8abbb1e4bcdc711fb333a8da
eclipse <eclipses.na.terra@gmail.com>
2026-08-17 21:35:36 -0300

    Update match_runner.py

--- match_runner.py
+++ match_runner.py
@@ -1,7 +1,7 @@
 import chess as c,chess.engine as e
 def run_match(f,o,we,wh,wt,wd,be,bh,bt,bd,pn):
  Wd,Bd,b,v,l=sorted(wd,reverse=True),sorted(bd,reverse=True),c.Board(f),0,[]
- [l.append(b.san(m)) or b.push(m) for m in c.Board(f).parse_san_moves(o)]
+ [l.append(b.san(m)) or b.push(m) for m in c.Board(f).parse_san_moves(" ".join([x for x in o.split() if not x.endswith(".") and not x[:-1].isdigit()]))]
  with e.SimpleEngine.popen_uci(we) as W,e.SimpleEngine.popen_uci(be) as B:
   W.configure({"Hash":wh,"Threads":wt});B.configure({"Hash":bh,"Threads":bt});Wn,Bn=W.id["name"],B.id["name"]
   E,T=(B,W),(Bd,Wd)
